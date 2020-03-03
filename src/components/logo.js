28d05bf13e497e4ab399781e02c35728c3b5743c
melissalee0609 <49824396+melissalee0609@users.noreply.github.com>
2020-03-03 15:38:20 +0800

    3/3 melissa

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,14 +1,26 @@
 import React from 'react'
-//import logoImg from './job.png'
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import {makeStyles} from '@material-ui/core/styles';
+import { Avatar } from '@material-ui/core';
+import { AccountCircle } from '@material-ui/icons';
 
+const useStyles = makeStyles(theme => ({
+      
+    large: {
+      width: theme.spacing(15),
+      height: theme.spacing(15),
+    marginTop: theme.spacing(3), 
+    marginLeft: theme.spacing(50), 
+    marginBottom:theme.spacing(3),
+    variant: "middle",
+    },
+  }));
 
 export default function Logo(){
-   
+   const classes = useStyles();
         return (
-            <div className="logo-container">
-                <img className="logo-img" src={AccountCircleIcon} alt=""/>
-            </div>
+            <Avatar className={classes.large} variant='circle' style={{alignSelf: 'center'}}>
+                <AccountCircle />
+            </Avatar>
         )
     }
 
