4db6df9aa0db6f18e802891a949045396571dc52
cycsponge <lisa293341@gmail.com>
2020-05-16 15:43:54 +0800

    點名用好開關

--- src/components/teacher/rollcall/GPS/GPS.js
+++ src/components/teacher/rollcall/GPS/GPS.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import Backdrop from '@material-ui/core/Backdrop';
-import CloseIcon from '@material-ui/icons/Close';
-import Slide from '@material-ui/core/Slide';
-import IconButton from '@material-ui/core/IconButton';
+import axios from 'axios';
 import ComButton from "../../../ComButton";
-import Grid from '@material-ui/core/Grid';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import {useParams} from "react-router-dom";
 import { usePosition } from 'use-position';
-import axios from 'axios';
+import MuiAlert from "@material-ui/lab/Alert";
+import CloseIcon from '@material-ui/icons/Close';
+import { makeStyles } from '@material-ui/core/styles';
+import {IconButton, Snackbar, Toolbar, AppBar, Grid, Slide, Backdrop, Dialog, Button} from "@material-ui/core";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -51,71 +45,81 @@ const useStyles = makeStyles(theme => ({
 }));
 /*---------------------------------------------*/
 
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 export default function GPS() {
-    const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
-
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  // 成功小綠綠
+  const [openS, setOpenS] = React.useState(false);
+  // 失敗小紅1
+  const [openErr1, setOpenErr1] = React.useState(false);
+  const [clicked, setClicked] = React.useState(true);
   const params = useParams();
   // console.log(params);
 
 
   const watch = true;
-      const {
-        latitude,
-        longitude,
-        // error,
-      } = usePosition(watch);
+    const {
+      latitude,
+      longitude,
+      // error,
+    } = usePosition(watch);
 
-
-      const [rcid, setRcid] = React.useState(0)
+  const [rcid, setRcid] = React.useState(0)
 
       const handleSubmit = () => {
         // setQrcode(uuidv4());
-        
-    fetch('/teacher/rollcall/addrollcall',{
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
+              
+          fetch('/teacher/rollcall/addrollcall',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                
+                // rc_inputsource:inputs.way,
+                qrcode: "1",
+                // @ts-ignore
+                cs_id: params.cs_id,
+                rc_inputsource: 'GPS點名',
+                gps_point: latitude + "," + longitude,
+            })
+        })
+        .then(res => {
+                          
+          async function fetchres(){
+          const rq = await res.text();  //接收後端傳來的訊息
+          if (rq === 'request failed. teacher not in this class!')
+          {
+              //alert("點名失敗! 您不是此課程的老師!");
+              setOpenErr1(true);
+              setClicked(false);
+              console.log(1);
+              
+          }
+          else if(rq === "request successful! the rollcall has already added!") 
+          {
+              //alert("點名成功!");
+              setOpenS(true);
+              setClicked(false);
+              console.log(2);
+              // setQrcode(null);   
+          }
           
-          // rc_inputsource:inputs.way,
-          qrcode: "1",
-          // @ts-ignore
-          cs_id: params.cs_id,
-          rc_inputsource: 'GPS點名',
-          gps_point: latitude + "," + longitude,
-      })
-  })
-  .then(res => {
-                    
-    async function fetchres(){
-    const rq = await res.text();  //接收後端傳來的訊息
-    if (rq === 'request failed. teacher not in this class!')
-    {
-        alert("點名失敗! 您不是此課程的老師!");
-        console.log(1);
+          
+      } fetchres() })
+        .then(res => {
+        async function fetchData() {
+          const result = await axios.get(`/teacher/rollcall/findRCID/1/`)
+          setRcid(result.data[0]["rc_id"]);
         
-    }
-    else if(rq === "request successful! the rollcall has already added!") 
-    {
-        alert("點名成功!");
-        console.log(2);
-        // setQrcode(null);   
-    }
-    
-    
-} fetchres() })
-  .then(res => {
-  async function fetchData() {
-    const result = await axios.get(`/teacher/rollcall/findRCID/1/`)
-    setRcid(result.data[0]["rc_id"]);
-  
-    console.log(result.data[0]["rc_id"]);
-    }
-    fetchData()
-})
+          console.log(result.data[0]["rc_id"]);
+          }
+          fetchData()
+      })
     
 
   }
@@ -128,6 +132,7 @@ export default function GPS() {
 
   const handleClose = () => {
     setOpen(false);
+    setClicked(true);
    
     
     // .then(res => {
@@ -146,6 +151,10 @@ export default function GPS() {
         })
     })
   };
+  const ErrClose = () => {
+    setOpenS(false);
+    setOpenErr1(false);
+  };
 
   
   const [inputs, setInputs] = React.useState({
@@ -160,7 +169,6 @@ export default function GPS() {
     setInputs(inputs => ({...inputs, [user]: event.target.value}));
     //不知道怎麼解釋哈哈哈哈
 }
-
 
   return (
     <div>
@@ -179,34 +187,41 @@ export default function GPS() {
     </IconButton>  
     </Toolbar>
     </AppBar>
-
-      
       <Grid
         container
         direction="column"
         justify="center"
         alignItems="center"
       >
-        <Grid item xs={12}>
-        <img
-                className={classes.imageSrc}
-                alt="complex"
-                src="https://image.flaticon.com/icons/svg/1321/1321801.svg"
-          />
-        </Grid>
-
-    <Grid item  xs={12}>
-      <Button onClick={handleSubmit}  className={classes.button}>
-        點名
-      </Button>
-    </Grid>    
-
-      
+          <Grid item xs={12}>
+          <img
+                  className={classes.imageSrc}
+                  alt="complex"
+                  src="https://image.flaticon.com/icons/svg/1321/1321801.svg"
+            />
+          </Grid>
+          <Grid item  xs={12}>
+            <Button disabled={clicked===false} onClick={handleSubmit}  className={classes.button}>
+              點名
+            </Button>
+          </Grid>    
         </Grid>
       </Backdrop>
 
         
       </Dialog>
+      {/* 成功小綠框 */}
+      <Snackbar open={openS} autoHideDuration={2000} onClose={ErrClose} style={{marginBottom:100}}>
+        <Alert severity="success">
+          點名成功！
+        </Alert>
+      </Snackbar>
+      {/* 失敗小紅框1 */}
+      <Snackbar open={openErr1}　autoHideDuration={2000} onClose={ErrClose} style={{marginBottom:100}}>
+        <Alert severity="error">
+          點名失敗！您不是此課程的老師！
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
