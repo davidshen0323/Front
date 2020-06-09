6306aae24fff6dee524fd3372dbda311592fe6ab
davidshen0323 <50398689+davidshen0323@users.noreply.github.com>
2020-06-09 20:58:27 +0800

    Update ING.js

--- src/components/student/applyrecord/ING/ING.js
+++ src/components/student/applyrecord/ING/ING.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import axios from 'axios';
-import Write from './Write';
-import { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useParams } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TableSortLabel} from "@material-ui/core";
-
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Write from './Write';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import {useEffect} from 'react';
+import {Paper} from '@material-ui/core';
 
 
 function descendingComparator(a, b, orderBy) {//順序升降
@@ -99,9 +106,6 @@ const useStyles = makeStyles(theme => ({
   root: {
     width: '100%',
   },
-  paper: {
-    width: '100%',
-  },
   table: {
     minWidth: 750,
   },
@@ -116,10 +120,6 @@ const useStyles = makeStyles(theme => ({
     top: 20,
     width: 1,
   },
-  container: {
-    display: 'flex',
-  },
-  
 }));
 /*---------------------------------------------*/
 
@@ -161,7 +161,7 @@ export default function Ing() {
   
    useEffect(() => {
      async function fetchData() {
-       const result = await axios.get(`/student/takeleave/TakeleaveRecord/${params.cs_id}/0/`);
+       const result = await axios.get(`/student/takeleave/TakeleaveRecord/${params.cs_id}/`);
        
        console.log(result.data);
        
@@ -172,7 +172,7 @@ export default function Ing() {
 
   return (
     <div className={classes.root}>  
-          
+      <Paper>
         <TableContainer>
           
           <Table
@@ -197,10 +197,10 @@ export default function Ing() {
                       <TableCell></TableCell>
                       {
                     ingList.map( (list, i) =>   i < 4 ? 
-                    <TableCell height="30" key={i} component="th" scope="row" align="left" padding="none" >
+                    <TableCell key={i} component="th" scope="row" align="left" padding="none" >
                     {ing[list]}
                  </TableCell>:
-                 <TableCell height="30" key={i} align="left" padding="none" >
+                 <TableCell key={i} align="left" padding="none" >
                    <Write 
                    id={ing['rc_id']}
                    time={ing['rc_starttime']}
@@ -229,6 +229,7 @@ export default function Ing() {
           onChangePage={handleChangePage}
           onChangeRowsPerPage={handleChangeRowsPerPage}
         />
+        </Paper>
     </div>
   );
 }
